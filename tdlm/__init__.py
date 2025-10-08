1dcf39bf478c192819027d61267b116b8c9ed74c
skjerns <14980558+skjerns@users.noreply.github.com>
2025-10-08 10:24:55 +0200

    fix omports

--- tdlm/__init__.py
+++ tdlm/__init__.py
@@ -7,5 +7,6 @@ Created on Wed Mar 27 15:09:29 2024
 
 from .core import *
 from .plotting import plot_sequenceness, plot_permutation_distribution
+from .plotting import plot_tval_distribution
 from . import plotting
 from . import utils
