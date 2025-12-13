8dc082ecb436ba53dec9a0bd74e3ceb47731dd5b
Simon Kern <14980558+skjerns@users.noreply.github.com>
2025-12-13 15:23:32 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(name='tdlm-python',
       author_email='nomail@nomail.com',
       license='GPLv4',
       packages=['tdlm'],
-      install_requires=['numpy', 'numba', 'seaborn'],
+      install_requires=['numpy', 'numba', 'seaborn', 'tqdm'],
       zip_safe=False,
       classifiers=[
           "Programming Language :: Python :: 3",
