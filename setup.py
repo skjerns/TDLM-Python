07aa6df5802a303e7b9a792d2c3e9cd150a6f537
Simon Kern <14980558+skjerns@users.noreply.github.com>
2025-12-18 10:57:10 +0100

    merge

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from setuptools import setup
 
 setup(name='tdlm-python',
@@ -16,3 +17,23 @@ setup(name='tdlm-python',
           "Programming Language :: Python :: 3",
           "Operating System :: OS Independent",
       ],)
+=======
+from setuptools import setup
+
+setup(name='tdlm-python',
+      version='0.4',
+      description='TDLM implementation for Python',
+      long_description='Temporally delayed linear modelling is a way to quantify sequential occurrences of events in time series and biosignals such as EEG or MEG',
+      long_description_content_type="text/markdown",
+      url='http://github.com/skjerns/TDLM-Python',
+      author='skjerns',
+      author_email='nomail@nomail.com',
+      license='GPLv4',
+      packages=['tdlm'],
+      install_requires=['numpy', 'numba', 'seaborn', 'tqdm', 'joblib'],
+      zip_safe=False,
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "Operating System :: OS Independent",
+      ],)
+>>>>>>> 33cf1d1f5a51521d29acfe59c96b467c25bd9801
